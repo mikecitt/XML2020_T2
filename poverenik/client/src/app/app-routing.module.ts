e9727ea3339f4563650c266b2bf7ce36a48b4201
Petar Cerovic <quadrition@gmail.com>
2021-02-06 00:31:01 +0100

    Added register functionality

--- poverenik/client/src/app/app-routing.module.ts
+++ poverenik/client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginGuard } from './guards/login-guard.guard';
+import { RegisterGuard } from './guards/register-guard.guard';
 import { HomePageComponent } from './modules/home/home-page/home-page.component';
 import { LoginPageComponent } from './modules/login/login-page/login-page.component';
 import { RegisterPageComponent } from './modules/register/register-page/register-page.component';
@@ -8,7 +9,11 @@ import { RegisterPageComponent } from './modules/register/register-page/register
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: LoginPageComponent, canActivate: [LoginGuard] },
-  { path: 'register', component: RegisterPageComponent },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+    canActivate: [RegisterGuard],
+  },
 ];
 
 @NgModule({
--- poverenik/client/src/app/services/auth/authentication.service.ts
+++ poverenik/client/src/app/services/auth/authentication.service.ts
@@ -32,6 +32,25 @@ export class AuthenticationService {
     );
   }
 
+  register(
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string
+  ) {
+    const body = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                  <kor:korisnik xmlns:kor="http://localhost:8080/korisnici">
+                      <kor:email_adresa>${email}</kor:email_adresa>
+                      <kor:sifra>${password}</kor:sifra>
+                      <kor:ime>${firstName}</kor:ime>
+                      <kor:prezime>${lastName}</kor:prezime>
+                  </kor:korisnik>`;
+
+    return this.http.post('http://localhost:8080/auth/register', body, {
+      observe: 'response',
+    });
+  }
+
   setLoginCredentials(token: string, role: string) {
     localStorage.setItem('token', token);
     localStorage.setItem('role', role);
