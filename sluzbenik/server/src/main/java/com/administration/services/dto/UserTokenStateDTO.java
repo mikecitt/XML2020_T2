34be4209f9a01668a0e3456e25f32d954c312e17
Mikec <mikecitt@gmail.com>
2021-02-05 15:31:58 +0100

    Dodat korisnicka rola u tokenStateDTO sluzbenik.

--- sluzbenik/server/src/main/java/com/administration/services/dto/UserTokenStateDTO.java
+++ sluzbenik/server/src/main/java/com/administration/services/dto/UserTokenStateDTO.java
@@ -3,25 +3,26 @@ package com.administration.services.dto;
 import javax.xml.bind.annotation.*;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "", propOrder = {
-        "accessToken",
-        "expiresIn"
-})
+@XmlType(name = "", propOrder = { "accessToken", "expiresIn", "role" })
 @XmlRootElement(name = "token")
 public class UserTokenStateDTO {
     @XmlElement(name = "accessToken", required = true)
     private String accessToken;
     @XmlElement(name = "expiresIn", required = true)
     private Long expiresIn;
+    @XmlElement(name = "role", required = true)
+    private String role;
 
     public UserTokenStateDTO() {
         this.accessToken = null;
         this.expiresIn = null;
+        this.role = null;
     }
 
-    public UserTokenStateDTO(String accessToken, long expiresIn) {
+    public UserTokenStateDTO(String accessToken, long expiresIn, String role) {
         this.accessToken = accessToken;
         this.expiresIn = expiresIn;
+        this.role = role;
     }
 
     public String getAccessToken() {
@@ -38,5 +39,13 @@ public class UserTokenStateDTO {
 
     public void setExpiresIn(Long expiresIn) {
         this.expiresIn = expiresIn;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
