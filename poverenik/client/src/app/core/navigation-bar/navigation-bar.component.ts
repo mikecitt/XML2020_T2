392b7b94fe05bc85d21fbad028e7cd93d8e1d4e6
Petar Cerovic <quadrition@gmail.com>
2021-02-06 00:41:53 +0100

    Promenjene akcije u nav bar-u

--- poverenik/client/src/app/core/navigation-bar/navigation-bar.component.ts
+++ poverenik/client/src/app/core/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/auth/authentication.service';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.css']
+  styleUrls: ['./navigation-bar.component.css'],
 })
 export class NavigationBarComponent implements OnInit {
+  constructor(private authService: AuthenticationService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get isLogged() {
+    return this.authService.isLogged();
   }
 
+  logout() {
+    this.authService.logout();
+  }
+
+  ngOnInit(): void {}
 }
