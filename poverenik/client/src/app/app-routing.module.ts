02d00363f64d3e82eaf96e30044086f24222c92f
Petar Cerovic <quadrition@gmail.com>
2021-02-05 22:57:36 +0100

    Added login guard

--- poverenik/client/src/app/app-routing.module.ts
+++ poverenik/client/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginGuard } from './guards/login-guard.guard';
 import { HomePageComponent } from './modules/home/home-page/home-page.component';
 import { LoginPageComponent } from './modules/login/login-page/login-page.component';
 import { RegisterPageComponent } from './modules/register/register-page/register-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'login', component: LoginPageComponent },
+  { path: 'login', component: LoginPageComponent, canActivate: [LoginGuard] },
   { path: 'register', component: RegisterPageComponent },
 ];
 
