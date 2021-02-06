44108a012de6c4398633903441007a878f370e41
Petar Cerovic <quadrition@gmail.com>
2021-02-06 15:30:23 +0100

    Added pdf loading

--- poverenik/client/src/app/modules/complaint-decision/overview/overview.component.ts
+++ poverenik/client/src/app/modules/complaint-decision/overview/overview.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { ComplaintDecisionService } from 'src/app/services/complaint-decision/complaint-decision.service';
 
 @Component({
   selector: 'app-complaint-decision-overview',
@@ -9,7 +11,42 @@ export class ComplaintDecisionOverviewComponent implements OnInit {
   @Input()
   complaint: any;
 
-  constructor() {}
+  pdfLoading: boolean = false;
+
+  constructor(
+    private service: ComplaintDecisionService,
+    private notification: NzNotificationService
+  ) {}
+
+  openComplaintPdf() {
+    this.pdfLoading = true;
+
+    let id = this.complaint.about.substring(
+      this.complaint.about.lastIndexOf('/') + 1
+    );
+
+    this.service
+      .getPdf(id)
+      .subscribe(
+        (response) => {
+          if (response.body !== null) {
+            let file = new Blob([response.body], { type: 'application/pdf' });
+            var fileURL = URL.createObjectURL(file);
+            window.open(fileURL);
+          }
+        },
+        () => {
+          this.notification.create(
+            'error',
+            'Error',
+            'There was an error in the server'
+          );
+        }
+      )
+      .add(() => {
+        this.pdfLoading = false;
+      });
+  }
 
   ngOnInit(): void {}
 }
